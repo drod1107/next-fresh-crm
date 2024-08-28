accda5756a329c04d4ce8bd8393e96e1e3ec0383
David Rodriguez <80010850+drod1107@users.noreply.github.com>
2024-08-28 15:33:09 -0500

    add umami tracker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 //  src > app > layout.tsx
 
+import Script from "next/script";
 import { ClerkProvider } from '@clerk/nextjs';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from "../theme";
@@ -12,12 +13,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        async
+        strategy="beforeInteractive"
+        data-website-id="6259ab30-70a8-401d-9be4-d91684bb3c13"
+        src="https://umami-windrose.vercel.app/script.js"
+      />
       <body>
-      <ClerkProvider afterSignOutUrl={"/"}>
-        <ThemeProvider theme={theme}>
-          {children}
-        </ThemeProvider>
-      </ClerkProvider>
+        <ClerkProvider afterSignOutUrl={"/"}>
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
